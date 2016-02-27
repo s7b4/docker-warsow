2baa171b177db11dc8bc682b2c5440d22fa9cf00
Stephane BARON <baron.stephane@gmail.com>
2016-02-27 23:49:26 +0100

    Gestion basique de la configuration

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -8,9 +8,25 @@ if [ ! -f "$WS_HOME/wsw_server" ]; then
 	curl -s "$WARSOW_URL" | tar xzf - --directory "$WS_HOME" --strip-components 1
 fi
 
+# Configuration
+if [ ! -f "/etc/warsow/$LOAD_CONFIG.cfg" ]; then
+	echo >&2 "error: missing file: /etc/warsow/$LOAD_CONFIG.cfg"
+	exit 1
+fi
+
+# Options de jeu
+grep -e "^set .* \".*\"" /etc/warsow/$LOAD_CONFIG.cfg | while read config_line;
+do
+	CONFIG_KEY=$(echo $config_line | sed -r 's/set (\w+) "(.*)"/\1/');
+	CONFIG_VALUE=$(echo $config_line | sed -r 's/set (\w+) "(.*)"/\2/');
+	sed -i "s/\(set $CONFIG_KEY \).*/\1\"$CONFIG_VALUE\"/" $WS_HOME/basewsw/dedicated_autoexec.cfg
+done;
+
 # Droits sur volume
 chown -R "$WS_USER":"$WS_USER" "$WS_HOME"
 
+# Arch (from warsow)
+ARCH=$(uname -m | sed -e s/i.86/i386/ -e s/sun4u/sparc/ -e s/sparc64/sparc/ -e s/arm.*/arm/ -e s/sa110/arm/ -e s/alpha/axp/)
+
 # Démarrage
-cd "$WS_HOME"
-exec gosu "$WS_USER" "$WS_HOME/wsw_server.x86_64" +set fs_basepath "$WS_HOME" +set fs_usehomedir "0" "${@}"
+exec gosu "$WS_USER" "$WS_HOME/wsw_server.$ARCH" +set fs_basepath "$WS_HOME" +set fs_usehomedir "0" "${@}"
